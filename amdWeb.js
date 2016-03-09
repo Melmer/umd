900f02de165686bf5456d1fbf34cc33cc303c81e
Melle Heeres <melle@wildsea.nl>
2016-03-09 16:06:04 +0100

    fixing return

--- amdWeb.js
+++ amdWeb.js
@@ -29,5 +29,9 @@
     // Just return a value to define the module export.
     // This example returns an object, but the module
     // can return a function as the exported value.
+
+
+    //we are returninggg
+
     return {};
 }));
